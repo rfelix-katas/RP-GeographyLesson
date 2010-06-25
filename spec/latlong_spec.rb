8082091e75aa8ee89515e21679f87394f781c6b5
Raoul Felix <rf@rfelix.com>
2010-06-25 02:54:23 +0100

    Changed way fixtur data is loaded and added failing specs for Decimal Degrees with Directions

--- spec/latlong_spec.rb
+++ spec/latlong_spec.rb
@@ -10,11 +10,17 @@ describe LatLong do
     context_name = nil
     Dir[File.dirname(__FILE__) + '/fixtures/*.txt'].each do |fixture|
     
-      context File.basename(fixture).gsub('.txt', '').gsub('_', ' ') do
+      context "using " + File.basename(fixture).gsub('.txt', '') do
         File.readlines(fixture).each do |line|
           parts = line.split(/\s+/)
-          input, x, y = parts[0] + " " + parts[1], parts[2].to_f, parts[3].to_f
-          it "#{input} should become [#{x}, #{y}]" do
+          case parts.size
+          when 4
+            input, x, y = "#{parts[0]} #{parts[1]}", parts[2].to_f, parts[3].to_f
+          when 6
+            input = "#{parts[0]} #{parts[1]} #{parts[2]} #{parts[3]}"
+            x, y  = parts[4].to_f, parts[5].to_f
+          end
+          it "'#{input}' should become [#{x}, #{y}]" do
             LatLong.to_xy(input).should == [x, y]
           end
         end
